dd48f4fdff10d7d1999ee7c2e5936acf04f7d132
LHJ <govl7844@gmail.com>
2024-10-14 18:14:27 +0900

    제조사랑제품

--- 20241014.sql
+++ 20241014.sql
@@ -26,3 +26,44 @@ SELECT PNAME, LENGTH(PNAME), LENGTHB(PNAME) FROM PERSON;
 SELECT * FROM PERSON;
 SELECT * FROM PERSON WHERE PAGE IS NULL;
 SELECT * FROM PERSON WHERE PAGE IS NOT NULL;
+
+
+
+-- 24.10.14실습
+-- 제조사 테이블 생성
+--CREATE TABLE Manufacturers (
+--    manufacturer_id NUMBER PRIMARY KEY,  -- 제조사번호
+--    manufacturer_name VARCHAR2(100)      -- 제조사명
+--);
+
+-- 제품 테이블 생성
+--CREATE TABLE Products (
+--    product_id NUMBER PRIMARY KEY,       -- 제품번호
+--    product_name VARCHAR2(100),          -- 제품명
+--    manufacturer_id NUMBER,              -- 제조사번호 (외래키)
+--    price NUMBER,                        -- 금액
+--    CONSTRAINT fk_manufacturer
+--        FOREIGN KEY (manufacturer_id) 
+--        REFERENCES Manufacturers(manufacturer_id)
+--);
+-- 제품 테이블 생성
+CREATE TABLE PRODUCTS (
+    PRODUCT_ID CHAR(10) PRIMARY KEY,        -- 제품번호
+    PRODUCT_NAME VARCHAR2(120),  -- 제품명
+    MANUFACTURER_ID CHAR(10),   -- 제조사번호
+    PRICE NUMBER,             -- 금액
+    FOREIGN KEY (MANUFACTURER_ID) REFERENCES MANUFACTURERS(MANUFACTURER_ID)
+);
+SELECT * FROM PRODUCTS;
+DROP TABLE PRODUCTS;
+
+-- 제조사 테이블 생성
+CREATE TABLE MANUFACTURERS (
+    MANUFACTURER_ID CHAR(10) PRIMARY KEY,   -- 제조사번호
+    MANUFACTURER_NAME VARCHAR2(60)  -- 제조사명
+);
+
+SELECT * FROM MANUFACTURERS;
+DROP TABLE MANUFACTURERS;
+
+
